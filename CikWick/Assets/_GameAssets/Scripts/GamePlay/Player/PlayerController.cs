a36dd1ce7631b9673956920a39f2ae254d371f38
Özgür Köker <ozgurkoker17@gmail.com>
2025-04-27 15:33:35 +0300

    v0.10 Boostable Spatula Added

--- CikWick/Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
+++ CikWick/Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
@@ -200,6 +200,11 @@ public class PlayerController : MonoBehaviour
     {
         _jumpForce = _startingJumpForce;
     }
+
+    public Rigidbody GetPlayerRigidbody()
+    {
+        return _playerRigidbody;
+    }
     #endregion
 
 }
--- CikWick/Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
+++ CikWick/Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PlayerInteractionController : MonoBehaviour
 {
+    private PlayerController _playerController;
+
+    private void Awake()
+    {
+        _playerController = GetComponent<PlayerController>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.TryGetComponent<ICollectible>(out var collectible))
@@ -9,4 +15,12 @@ public class PlayerInteractionController : MonoBehaviour
             collectible.Collect();
         }
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<IBoostable>(out var boostable))
+        {
+            boostable.Boost(_playerController);
+        }
+    }
 }
+
