b5b55d8e91a067cf86f92ccf2923d1bc1b72570d
Özgür Köker <ozgurkoker17@gmail.com>
2025-05-08 19:43:14 +0300

    v0.14.1 When the character was fast, collectible objects could be collected more than once until they were destroyed. Fixed

--- CikWick/Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
+++ CikWick/Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
@@ -13,6 +13,8 @@ public class PlayerInteractionController : MonoBehaviour
         if (other.gameObject.TryGetComponent<ICollectible>(out var collectible))
         {
             collectible.Collect();
+            //In cases where the character was fast, the object could not destroy itself immediately, and the trigger was triggered more than once, causing an error.
+            other.enabled = false;
         }
     }
     private void OnCollisionEnter(Collision collision)
