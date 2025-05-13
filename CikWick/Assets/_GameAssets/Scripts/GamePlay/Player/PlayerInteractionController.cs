311236d535cdb0caed139d7ff21790559015147c
Özgür Köker <ozgurkoker17@gmail.com>
2025-05-13 18:54:58 +0300

    v0.18 Damageable Fires Added

--- CikWick/Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
+++ CikWick/Assets/_GameAssets/Scripts/GamePlay/Player/PlayerInteractionController.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class PlayerInteractionController : MonoBehaviour
 {
+
+    [SerializeField] private Transform _playerVisualTransform;
+
     private PlayerController _playerController;
+    private Rigidbody _playerRigidbody;
 
     private void Awake()
     {
         _playerController = GetComponent<PlayerController>();
+        _playerRigidbody = GetComponent<Rigidbody>();
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -23,6 +28,17 @@ public class PlayerInteractionController : MonoBehaviour
         {
             boostable.Boost(_playerController);
         }
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.TryGetComponent<IDamageable>(out var damageable))
+        {
+            damageable.GiveDamage(_playerRigidbody, _playerVisualTransform);
+        }
+
+
+
     }
 }
 
--- CikWick/Assets/_GameAssets/Scripts/Managers/HealthManager.cs
+++ CikWick/Assets/_GameAssets/Scripts/Managers/HealthManager.cs
@@ -1,9 +1,24 @@
+using System;
 using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
+    public static HealthManager Instance { get; private set; }
+
+    public event Action OnPlayerDeath;
+
+    [Header("References")]
+    [SerializeField] private PlayerHealthUI _playerHealthUI;
+
+
+    [Header("Settings")]
     [SerializeField] private int _maxHealth = 3;
     private int _currentHealth;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
@@ -15,11 +30,11 @@ public class HealthManager : MonoBehaviour
         if (_currentHealth > 0)
         {
             _currentHealth -= damageAmount;
-            //TODO: UI Animate Damage
+            _playerHealthUI.AnimateDamage();
 
-            if (_currentHealth < 0)
+            if (_currentHealth <= 0)
             {
-                //TODO: Player Dead
+                OnPlayerDeath?.Invoke();
             }
         }
     }
